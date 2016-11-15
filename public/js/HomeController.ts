3430a356db03a139d676d7e2363fe42d3cb4505f
Ron Bravo <ronbravo1701@gmail.com>
2016-11-15 10:22:34 -0700

    Updating the home page to pull in the posts.

--- public/js/HomeController.ts
+++ public/js/HomeController.ts
@@ -1,5 +1,30 @@
 namespace App {
     export class HomeController {
-        
+        static $inject = ['$http'];
+
+        private httpService;
+
+        public postList;
+
+        constructor ($http: angular.IHttpService) {
+            this.httpService = $http;
+
+            // Auto load the posts as soon as this Object
+            // is created.
+            this.getPostList ();
+        }
+
+        public getPostList () {
+            this.httpService ({
+                url: '/posts',
+                method: 'GET'
+            })
+            .success ((response) => {
+                console.log ('This is the response: ', response);
+                this.postList = response;
+            })
+            .error ((response) => {
+            })
+        }
     }
 }
